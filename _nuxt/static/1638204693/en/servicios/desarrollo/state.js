976fca529f1f73056e4cbf1177bafe30075c76a6
margarcuae <margarcuae@gmail.com>
2021-11-29 13:59:13 -0300

    test2

--- _nuxt/static/1638204693/en/servicios/desarrollo/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"paulonia-view\u002F_nuxt\u002Fstatic\u002F1638204693",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fservicios\u002Fdesarrollo",config:{_app:{basePath:"paulonia-view\u002F",assetsPath:"paulonia-view\u002F_nuxt\u002F",cdnURL:a}}}}(null));
