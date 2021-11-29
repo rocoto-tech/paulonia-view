ce2af9778c54412f729c3ba240959155c96402ad
margarcuae <margarcuae@gmail.com>
2021-11-29 13:52:27 -0300

    ghj

--- README.md
+++
@@ -1,2 +0,0 @@
-# paulonia-view
-Pauloni.dev
---
+++ _nuxt/static/1638204693/servicios/diseno/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"paulonia-view\u002F_nuxt\u002Fstatic\u002F1638204693",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservicios\u002Fdiseno",config:{_app:{basePath:"paulonia-view\u002F",assetsPath:"paulonia-view\u002F_nuxt\u002F",cdnURL:a}}}}(null));
