d4e4bfc8b0481fa69cd43d49c6609b93f5b27065
margarcuae <margarcuae@gmail.com>
2021-12-01 23:19:49 -0300

    update some fizz

---
+++ _nuxt/static/1638411526/servicios/desarrollo/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpaulonia-view\u002F_nuxt\u002Fstatic\u002F1638411526",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservicios\u002Fdesarrollo",config:{_app:{basePath:"\u002Fpaulonia-view\u002F",assetsPath:"\u002Fpaulonia-view\u002F_nuxt\u002F",cdnURL:a}}}}(null));
