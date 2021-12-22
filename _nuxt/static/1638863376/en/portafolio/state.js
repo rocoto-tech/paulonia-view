859462eb6472408c0f9d2014c9709a836f7b3f8e
Luis <luisbch9@gmail.com>
2021-12-22 16:26:44 -0500

    update

--- _nuxt/static/1638863376/en/portafolio/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpaulonia-view\u002F_nuxt\u002Fstatic\u002F1638863376",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fportafolio",config:{_app:{basePath:"\u002Fpaulonia-view\u002F",assetsPath:"\u002Fpaulonia-view\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1638863376/en/servicios/diseno/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/en/servicios/diseno", {data:[{}],fetch:{},mutations:void 0});
