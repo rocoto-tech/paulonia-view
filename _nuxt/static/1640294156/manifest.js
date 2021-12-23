b8dd048563ffcd457cd1dbe82a3e9abdff305666
margarcuae <m234924@dac.unicamp.br>
2021-12-23 16:16:43 -0500

    update

---
+++ _nuxt/static/1640294156/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fservicios\u002Fdesarrollo","\u002Fen\u002Fservicios\u002Fdesarrollo","\u002Fcontacto","\u002Fportafolio","\u002Fen\u002Fservicios\u002Fdiseno","\u002Fen\u002Fcontacto","\u002Fen\u002Fportafolio","\u002Fservicios\u002Fdiseno","\u002Fen"]})
